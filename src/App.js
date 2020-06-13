367cf74e061806b95934d7500eceddbb18e7e23b
MUHAMMAD AZHAR <azharthegeek@gmail.com>
2020-06-14 02:20:42 +0500

    React Context demo parent child

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Parent from './Parent';
+import ValueContext from './ValueContext';
+
+function App() {
+  //let [number, setNumber] = useState(45);
+  let value = useState(48);
+  return (
+    <ValueContext.Provider value={value}>
+      <div >
+        Hello World
+        <Parent></Parent>
+      </div>
+    </ValueContext.Provider>
+    
+  );
+}
+
+export default App;
